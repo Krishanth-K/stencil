dfa3fec4def3397f5114ab539d39b9d12cd1b278
Krishanth K <krishanth.k2006@gmail.com>
2025-12-30 23:37:00 +0530

    started ast prep

--- stencil/cli.py
+++ stencil/cli.py
@@ -1,9 +1,9 @@
 import argparse
 from pathlib import Path
-import yaml
 import json
 import sys
-from stencil.main import run
+import yaml
+from stencil.main import run, generate_tree
 
 CONFIG_FILES = ["stencil.yaml", "stencil.json"]
 
@@ -16,20 +16,27 @@ def find_config():
 
 def main():
     parser = argparse.ArgumentParser(description="Generate files from stencil.yaml/json")
+
     parser.add_argument("-w", "--watch", action="store_true", help="Watch config and regenerate automatically")
     parser.add_argument("-b", "--backend", type=str, default="html", help="The backend to use (html, imgui)")
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     config_path = find_config()
+
     if not config_path:
         print("Error: stencil.yaml or stencil.json not found.", file=sys.stderr)
         return 1
 
+    # Read and parse the config file
     with open(config_path) as f:
         if config_path.suffix == ".yaml":
             config_data = yaml.safe_load(f)
         else:
             config_data = json.load(f)
 
-    run(config_data, args)
+    # run(config_data, args)
+    generate_tree(config_data)
 
+    return 0
+
+main()
--- stencil/main.py
+++ stencil/main.py
@@ -1,19 +1,52 @@
+from pprint import pprint
 from stencil.html_backend import generate_html
 from stencil.imgui_backend import generate_imgui
 
 def run(config_data, args):
+
     backend = args.backend
     if backend == 'html' and 'backend' in config_data:
         backend = config_data['backend']
 
     if backend == "html":
+
         print("Using html backend")
         html_code = generate_html(config_data)
+
         with open("index.html", "w") as f:
             f.write(html_code)
         print("HTML generated at index.html")
+
+
     elif backend == "imgui":
+
+        print("Using imgui backend")
         imgui_code = generate_imgui(config_data)
+
         with open("ui.py", "w") as f:
             f.write(imgui_code)
+
         print("ImGui code generated at ui.py")
+
+
+#TEST: testing
+config_data = {'app': [{'title': 'Admin Dashboard'},
+         {'text': 'Welcome to the central control panel. Please select an '
+                  'option below to proceed.'},
+         {'button': {'callback': 'onViewAnalytics', 'label': 'View Analytics'}},
+         {'button': {'callback': 'onManageUsers', 'label': 'Manage Users'}},
+         {'button': {'callback': 'onSystemSettings',
+                     'label': 'System Settings'}},
+         {'text': '© 2025 Admin Corp.'}],
+ 'config': {'author': 'Admin Team', 'backend': 'html', 'version': '1.0.0'}}
+
+
+
+def generate_abstract_tree(ui_data):
+    
+    def generate(node):
+
+    # walk the tree
+    for element in ui_data["app"]:
+
+        if isins
